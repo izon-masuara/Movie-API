f0772b2d7d2abd84bf0469612baa5bbff04c65b3
kautsar_masuara <kautsarmasuara@gmail.com>
2022-03-16 23:06:16 +0800

    unit test create user

--- services/user/_test_/user.test.js
+++ services/user/_test_/user.test.js
@@ -2,11 +2,19 @@ import { app } from '../src/index'
 import request from 'supertest'
 
 describe('GET /users', () => {
+    const user1 = {
+        email : `test@mail.com`,
+        password : `test1234`,
+        status : `active`,
+        expired : `2022-01-01`,
+        role : `admin`,
+    }
+
     it('response get all users', done => {
         request(app)
             .get('/users')
-            .then(response => {
-                const { status, body } = response
+            .then(resp => {
+                const { status, body } = resp
                 expect(status).toBe(200)
                 expect(body).toEqual(
                     expect.arrayContaining(body)
@@ -15,4 +23,15 @@ describe('GET /users', () => {
             })
             .catch(err => done(err))
     });
+    it(`Success create user`, done => {
+        request(app)
+            .post(user1)
+            .then(resp => {
+                const { status,body } = resp
+                expect(status).toBe(201)
+                expect(body).toBe(`Email with name ${user1.email} created`)
+                done();
+            })
+            .catch(err => done(err))
+    })
 });
