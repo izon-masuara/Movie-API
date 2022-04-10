6b3db1bcace8ea328efc186bbacebab3943e41fd
kautsar_masuara <kautsarmasuara@gmail.com>
2022-04-10 20:10:03 +0800

    done to develop patch data, router like and router to get image form mongodb

--- services/movie/src/routers/index.js
+++ services/movie/src/routers/index.js
@@ -2,13 +2,19 @@ import express from 'express'
 import {
     getAll,
     createData,
-    updateData
+    patchData,
+    like,
+    destroy,
+    viewImage
 } from '../controllers'
 import { upload } from '../middlewares/multer'
 const router = express.Router()
 
 router.get('/', getAll)
 router.post('/', upload.single('image'), createData)
-router.patch('/:id', updateData)
+router.patch('/:id', patchData)
+router.patch('/like/:id', like)
+router.delete('/:id', destroy)
+router.get('/:id', viewImage)
 
 export default router
